b0a1a63bf3f40ee1998c0827bc5fa4c12073b3de
azita <azitaabdollahi75@gmail.com>
2023-12-29 15:07:57 +0330

    feat: create connection to mongodb utils.

---
+++ src/utils/mongoDBConnect/index.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const dbUrl = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASS}@${process.env.MONGO_IP}/${process.env.MONGO_DB}?authSource=admin`;
+
+async function connectDB() {
+  try {
+    mongoose.set('strictQuery', false);
+    await mongoose.connect(dbUrl);
+    console.log('database connected successfully...');
+  } catch (error) {
+    console.log("connection error...!\n");
+    console.error(error);
+    process.exit(1)
+  }
+};
+module.exports.connectDB = connectDB;
